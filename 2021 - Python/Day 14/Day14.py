a9f7a52e9be18247085786008b188f80567ebde0
Thomas White <tawhite@iinet.net.au>
2021-12-14 17:56:06 +0800

    2021 day 14

--- 2021 - Python/Day 14/Day14.py
+++ 2021 - Python/Day 14/Day14.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from tqdm import tqdm
 
 #from readFile import readFile
@@ -10,61 +12,52 @@ def readFile(file):
 
 def part1and2(input_lines, steps):
     print(input_lines)
-    pairs = {}
+    pairs = defaultdict(lambda: 0)
     template = input_lines[0]
     for ii in range(len(template) - 1):
         pair = template[ii] + template[ii + 1]
-        if pair in pairs:
-            pairs[pair] += 1
-        else:
-            pairs[pair] = 1
+        pairs[pair] += 1
 
     rules = {}
     for line in input_lines[2:]:
         pair, element = line.split(" -> ")
         rules[pair] = element
 
-    print(pairs)
-    newPairs = {}
-    for jj in tqdm(range(steps), unit="steps"):
+    print(dict(pairs))
+    newPairs = dict(pairs.copy())
+    for jj in range(steps):#tqdm(range(steps), unit="steps"):
         #for oldPair in tqdm(range(len(pairs)), leave=False, unit="pairs"):
-        newPairs = {}
+        #newPairs = {}
         for oldPair in pairs:
-            if pairs[oldPair] <= 0:
-                continue
+            #if pairs[oldPair] <= 0:
+            #    continue
 
             splitPairs = [oldPair[0] + rules[oldPair], rules[oldPair] + oldPair[1]]
 
             if oldPair in newPairs:
                 newPairs[oldPair] -= pairs[oldPair]
-            else:
-                newPairs[oldPair] = 0
 
             for pair in splitPairs:
                 if pair in newPairs:
-                    newPairs[pair] += 1
-                elif pair in pairs:
-                    newPairs[pair] = pairs[pair] * 2
+                    newPairs[pair] += pairs[oldPair]
                 else:
-                    newPairs[pair] = 1
-        pairs = newPairs.copy()
-        print(pairs)
+                    newPairs[pair] = pairs[oldPair]
 
-    letters = {}
+        pairs = newPairs.copy()
+        print(newPairs)
+
+    letters = defaultdict(lambda: 0)
     #{letter[0]: number if letter[0] in self.keys() else  for letter, number in pairs}
     for letter in pairs:
         number = pairs[letter]
-        if letter[0] in letters.keys():
-            letters[letter[0]] += number if number > 0 else 0
-        else:
-            letters[letter[0]] = number if number > 0 else 0
-    print(letters)
+        letters[letter[0]] += number if number > 0 else 0
+    print(dict(letters))
     elements = sorted(list(letters.values()))
-    print(elements[-1]-elements[0])
+    print(elements[-1]-elements[0]+1)
 
 if __name__ == '__main__':
-    test = 1
-    part = 1
+    test = 0
+    part = 2
 
     if test:
         inputLines = readFile("testInput.txt")
@@ -72,7 +65,7 @@ if __name__ == '__main__':
         inputLines = readFile("input.txt")
 
     if part == 1:
-        part1and2(inputLines, 4)
+        part1and2(inputLines, 10)
     elif part == 2:
         part1and2(inputLines, 40)
 
