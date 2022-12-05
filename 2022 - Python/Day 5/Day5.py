ef8c1a6a00c3648d342eca0fc55e0b6fd8876ba4
Thomas White <tawhite@iinet.net.au>
2022-12-05 16:20:56 +0800

    2022 day 5 (incomplete)

--- 2022 - Python/Day 5/Day5.py
+++ 2022 - Python/Day 5/Day5.py
@@ -12,28 +12,61 @@ def readFile(file):
 class Crate:
     crate_below = None
     crates_above = 0
-    creat_ID = ''
+    crate_ID = ''
 
     def __init__(self, ID):
-        self.creat_ID = ID
+        self.crate_ID = ID
+
+    def __str__(self):
+        return self.crate_ID
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Stack:
     top_crate = None
-    stack_height = 0
 
-    def add_creat(self, creat: Crate):
+    def __str__(self):
+        if not self.top_crate:
+            return
+
+        crate = self.top_crate
+        stack_str = ""
+        while crate is not None:
+            stack_str += f"[{crate.crate_ID}]\n"
+            crate = crate.crate_below
+
+        return stack_str
+
+    def __repr__(self):
+        return "\n" + self.__str__()
+
+    def add_crate(self, crate: Crate):
         if self.top_crate:
-            creat.crate_below = self.top_crate
+            crate.crate_below = self.top_crate
 
-        self.top_crate = creat
-        self.stack_height += 1
+        self.top_crate = crate
+
+    def remove_crate(self, crate: Crate):
+        self.top_crate = crate.crate_below
 
 
 class Move:
     number = 0
     start = 0
     end = 0
+
+    def __init__(self, values):
+        self.number = values[0]
+        self.start = values[1]
+        self.end = values[2]
+
+    def __str__(self):
+        return f"move {self.number} from {self.start} to {self.end}"
+
+    def __repr__(self):
+        return self.__str__()
 
 
 def part1(input_lines):
@@ -52,8 +85,6 @@ def part1(input_lines):
         else:
             move_input.append(line)
 
-    print(move_input)
-
     stacks = []
     stack_input = stack_input[:-1]
     stack_input.reverse()
@@ -66,10 +97,14 @@ def part1(input_lines):
 
             if crates_on_row[ii] != " ":
                 crate = Crate(crates_on_row[ii])
-                stacks[ii].add_creat(crate)
-
+                stacks[ii].add_crate(crate)
 
     moves = []
+    for row in move_input:
+        parsed_row = re.findall("move (\\d+?) from (\\d+?) to (\\d+?)", row)
+        moves.append(Move(parsed_row[0]))
+
+    for move in moves:
 
 
 def part2(input_lines):
