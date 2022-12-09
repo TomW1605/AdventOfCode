a846a8fe3a573589945b7f5f42ef039c04267125
Thomas White <tawhite@iinet.net.au>
2022-12-09 14:25:15 +0800

    2022 day 9 merge part 1 and 2

--- 2022 - Python/Day 9/Day9.py
+++ 2022 - Python/Day 9/Day9.py
@@ -48,67 +48,9 @@ def print_grid(knots: List[Knot], max_point, min_point):
         print()
 
 
-def part1(input_lines):
+def part_1_and_2(input_lines, knots):
     print(input_lines)
     visited = {Knot(0, 0)}
-    knots = [Knot(0, 0, "H"), Knot(0, 0, "T")]
-    max_point = [0, 0]
-    min_point = [0, 0]
-
-    for line in input_lines:
-        direction, distance = line.split()
-        for ii in range(int(distance)):
-            if direction == "R":
-                knots[0].x += 1
-            elif direction == "L":
-                knots[0].x -= 1
-            elif direction == "U":
-                knots[0].y += 1
-            elif direction == "D":
-                knots[0].y -= 1
-
-            if knots[0].x > max_point[0]:
-                max_point[0] = knots[0].x
-            if knots[0].y > max_point[1]:
-                max_point[1] = knots[0].y
-
-            if knots[0].x < min_point[0]:
-                min_point[0] = knots[0].x
-            if knots[0].y < min_point[1]:
-                min_point[1] = knots[0].y
-
-            #print(knots[0], knots[1])
-            #print_grid(knots, max_point, min_point)
-
-            if not adjacent(knots[0], knots[1]):
-                print("not adjacent")
-
-                if knots[1].x > knots[0].x:
-                    knots[1].x -= 1
-
-                if knots[1].y > knots[0].y:
-                    knots[1].y -= 1
-
-                if knots[1].x < knots[0].x:
-                    knots[1].x += 1
-
-                if knots[1].y < knots[0].y:
-                    knots[1].y += 1
-
-                #print_grid(knots, max_point, min_point)
-
-                visited.add(knots[1])
-            #print(visited)
-            #print()
-
-    print(len(visited))
-
-
-def part2(input_lines):
-    print(input_lines)
-    visited = {Knot(0, 0)}
-    knots = [Knot(0, 0, "H"), Knot(0, 0, "1"), Knot(0, 0, "2"), Knot(0, 0, "3"), Knot(0, 0, "4"),
-             Knot(0, 0, "5"), Knot(0, 0, "6"), Knot(0, 0, "7"), Knot(0, 0, "8"), Knot(0, 0, "9")]
     max_point = [0, 0]
     min_point = [0, 0]
 
@@ -174,7 +116,10 @@ if __name__ == '__main__':
         inputLines = readFile("input.txt")
 
     if part == 1:
-        part1(inputLines)
+        knots = [Knot(0, 0, "H"), Knot(0, 0, "T")]
+        part_1_and_2(inputLines, knots)
     elif part == 2:
-        part2(inputLines)
+        knots = [Knot(0, 0, "H"), Knot(0, 0, "1"), Knot(0, 0, "2"), Knot(0, 0, "3"), Knot(0, 0, "4"),
+                 Knot(0, 0, "5"), Knot(0, 0, "6"), Knot(0, 0, "7"), Knot(0, 0, "8"), Knot(0, 0, "9")]
+        part_1_and_2(inputLines, knots)
 
