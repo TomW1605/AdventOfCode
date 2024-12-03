cde19cbea90957e28dbeab1c4f3a55293abb010b
Thomas White <tomw1605@users.noreply.github.com>
2024-12-03 16:42:01 +0800

    2024 day 2 part 2

--- 2024 - Python/Day 02/Day02.py
+++ 2024 - Python/Day 02/Day02.py
@@ -24,30 +24,50 @@ def part1(input_lines):
             total_safe += 1
     print(total_safe)
 
+def check_report(report):
+    unsafe_levels = 0
+    if report[1] > report[0]:
+        report_inc = True
+    else:
+        report_inc = False
+    for ii in range(len(report)-1):
+        if report_inc:
+            if not (report[ii+1] > report[ii] and report[ii+1] - report[ii] <= 3):
+                unsafe_levels += 1
+                # break
+        else:
+            if not (report[ii+1] < report[ii] and report[ii] - report[ii+1] <= 3):
+                unsafe_levels += 1
+                # break
+    return unsafe_levels
+
 def part2(input_lines):
     reports = [list(map(int, report.split(" "))) for report in input_lines]
     total_safe = 0
+    unsafe_reports = []
     for report in reports:
-        unsafe_levels = 0
-        if report[1] > report[0]:
-            report_inc = True
-        else:
-            report_inc = False
-        for ii in range(len(report)-1):
-            if report_inc:
-                if not (report[ii+1] > report[ii] and report[ii+1] - report[ii] <= 3):
-                    unsafe_levels += 1
-                    # break
-            else:
-                if not (report[ii+1] < report[ii] and report[ii] - report[ii+1] <= 3):
-                    unsafe_levels += 1
-                    # break
-        if unsafe_levels <= 1:
+        unsafe_levels = check_report(report)
+        if unsafe_levels == 0:
             total_safe += 1
+        else:
+            unsafe_reports.append(report)
+    # print(unsafe_reports)
+    unsafe_reports_copy = unsafe_reports.copy()
+    for report in unsafe_reports:
+        for ii in range(len(report)):
+            report_copy = report.copy()
+            report_copy.pop(ii)
+            unsafe_levels = check_report(report_copy)
+            if unsafe_levels == 0:
+                unsafe_reports_copy.remove(report)
+                total_safe += 1
+                break
+
+    # print(unsafe_reports_copy)
     print(total_safe)
 
 if __name__ == '__main__':
-    test = 1
+    test = 0
     part = 2
 
     start_time = time.time()
