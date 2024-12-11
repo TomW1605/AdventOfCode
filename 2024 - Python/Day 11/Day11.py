438598202dd66d75c204431dda60d03502f741d1
Thomas White <tomw1605@users.noreply.github.com>
2024-12-11 16:45:55 +0800

    2024 day 11 part 2

--- 2024 - Python/Day 11/Day11.py
+++ 2024 - Python/Day 11/Day11.py
@@ -1,6 +1,5 @@
 import time
-
-import tqdm
+from collections import defaultdict
 
 def blink_pt1(stones):
     new_stones = stones.copy()
@@ -26,54 +25,30 @@ def part1(input_lines):
         # print(stones)
     print(len(stones))
 
-def blink_pt2(stones, zeros=0, ones=0, twenty_twenty_fours=0):
-    new_stones = [20, 24] * twenty_twenty_fours
-    twenty_twenty_fours = ones
-    ones = zeros
-    zeros = 0
-    for stone in stones:
+def blink_pt2(stones):
+    new_stones = defaultdict(int)
+    for stone in stones.keys():
         stone_str = str(stone)
-        if len(stone_str) % 2 == 0:
+        if stone == 0:
+            new_stones[1] += stones[0]
+        elif len(stone_str) % 2 == 0:
             first_half = int(stone_str[0:len(stone_str)//2])
+            new_stones[first_half] += stones[stone]
+
             second_half = int(stone_str[len(stone_str)//2:])
-
-            if first_half == 0:
-                zeros += 1
-            elif first_half == 1:
-                ones += 1
-            elif first_half == 2024:
-                twenty_twenty_fours += 1
-            else:
-                new_stones.append(first_half)
-
-            if second_half == 0:
-                zeros += 1
-            elif second_half == 1:
-                ones += 1
-            elif second_half == 2024:
-                twenty_twenty_fours += 1
-            else:
-                new_stones.append(second_half)
+            new_stones[second_half] += stones[stone]
         else:
-            new_stones.append(stone * 2024)
-    return new_stones, zeros, ones, twenty_twenty_fours
+            new_val = stone * 2024
+            new_stones[new_val] += stones[stone]
+    return new_stones
 
 def part2(input_lines):
-    stones = [int(stone) for stone in input_lines[0].split(" ")]
-    zeros = ones = twenty_twenty_fours = 0
-    # print(stones)
-    # for _ in range(75):
-    for _ in (t := tqdm.tqdm(range(75), unit="blinks", postfix={"stones": len(stones),
-                                                                "zeros": zeros,
-                                                                "ones": ones,
-                                                                "2024s": twenty_twenty_fours})):
-        stones, zeros, ones, twenty_twenty_fours = blink_pt2(stones, zeros, ones, twenty_twenty_fours)
-        t.set_postfix({"stones": len(stones),
-                       "zeros": zeros,
-                       "ones": ones,
-                       "2024s": twenty_twenty_fours})
+    stones = {int(stone): 1 for stone in input_lines[0].split(" ")}
+    print(stones)
+    for _ in range(75):
+        stones = blink_pt2(stones)
         # print(stones)
-    print(len(stones) + zeros + ones + twenty_twenty_fours)
+    print(sum(stones.values()))
 
 if __name__ == '__main__':
     test = 0
