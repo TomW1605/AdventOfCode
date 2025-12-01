95a4ba693d51025a3fe93ae2cf96ab7152d90a81
Thomas White <tomw1605@users.noreply.github.com>
2025-12-01 14:52:16 +0800

    setup 2025

---
+++ 2025 - Python/template.py
@@ -0,0 +1,31 @@
+import time
+
+def part1(input_lines):
+    print(input_lines)
+
+def part2(input_lines):
+    print(input_lines)
+
+if __name__ == '__main__':
+    test = 1
+    part = 1
+
+    start_time = time.time()
+
+    if test:
+        inputLines = open("testInput.txt", "r").read().splitlines()
+    else:
+        inputLines = open("input.txt", "r").read().splitlines()
+
+    if part == 1:
+        part1(inputLines)
+    elif part == 2:
+        part2(inputLines)
+
+    total_time = time.time() - start_time
+
+    hours = int(total_time / 3600)
+    minutes = int(total_time / 60)
+    seconds = total_time % 60
+
+    print(f"{hours:02.0f}:{minutes:02.0f}:{seconds:06.3f}")
